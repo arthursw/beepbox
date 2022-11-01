ed9b64dea8181b527b69f553d6cff682e669c1a4
Arthur <arthur.sw@gmail.com>
2022-11-01 22:34:39 +0100

    update

--- websocket/main.js
+++ websocket/main.js
@@ -1,16 +1,37 @@
-const WebSocket = require('ws');
+import { WebSocketServer } from 'ws';
+
 let port = 5000;
-const wss = new WebSocket.Server({ port: port });
+
+function heartbeat() {
+    this.isAlive = true;
+}
+
+const wss = new WebSocketServer({ port: port });
 
 wss.on('connection', function connection(ws) {
-    ws.on('message', function incoming(data) {
-        console.log(data)
+    ws.isAlive = true;
+    ws.on('pong', heartbeat);
+
+    ws.on('message', function message(data, isBinary) {
         wss.clients.forEach(function each(client) {
             if (client !== ws && client.readyState === WebSocket.OPEN) {
-                client.send(data);
+                client.send(data, { binary: isBinary });
             }
         });
     });
+});
+
+const interval = setInterval(function ping() {
+    wss.clients.forEach(function each(ws) {
+        if (ws.isAlive === false) return ws.terminate();
+
+        ws.isAlive = false;
+        ws.ping();
+    });
+}, 30000);
+
+wss.on('close', function close() {
+    clearInterval(interval);
 });
 
 console.log('Websocket port: ', port);
