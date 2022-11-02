97132ddcb2fd9a99e2d7f644391c24d806af04fe
Arthur <arthur.sw@gmail.com>
2022-11-02 13:55:26 +0100

    take into account changes which were applied by received messages

--- editor/Communication.ts
+++ editor/Communication.ts
@@ -17,14 +17,19 @@ let capitalize = (str: string) => {
 export class Communication {
 	username: string
 	socket: WebSocket
-	currentSong: Object
+	lastSentSong: Object
 	songIsSet = false
 	pingTimeout = 35000
 	updateTimeout = null
+	recievedChanges = []
 	
 	// ignoreWhenUpdated = false;
 
 	constructor(private _doc: SongDocument) {
+		let w: any = window
+		w._doc = _doc
+		w.communication = this
+
 		this.username = capitalize(adjectives[Math.floor(Math.random() * adjectives.length)]) + ' ' + animals[Math.floor(Math.random() * animals.length)];
 		this._doc.notifier.watch(this.whenUpdated);
 		let host = location.hostname == 'localhost' ? 'localhost:5000' : location.hostname + '/ws/';
@@ -45,41 +50,83 @@ export class Communication {
 		};
 
 		this.socket.onmessage = (event) => {
-			console.log('receive: ', event.data)
+			// console.log('receive: ', event.data)
 
 			try {
 				let message = JSON.parse(event.data);
 				if (message.type == 'change') {
-					let jsonObject: Object = this._doc.song.toJsonObject(true, 1, true);
+					// console.log('--- receive update:')
+					// w.detectHole()
+					let jsonObject: Object = this.getJsonSong();
+					// w.printNotes(jsonObject)
+					jsonObject = this.convertNotesToObject(jsonObject);
 					message.changes.forEach(function (change: any) {
 						DeepDiff.applyChange(jsonObject, true, change);
 					});
+					this.recievedChanges = this.recievedChanges.concat(message.changes)
 
-					console.log('change song from user ' + message.user, message.changes, jsonObject);
+					// console.log('change song from user ' + message.user, message.changes, jsonObject);
 
 					// let jsonString = JSON.stringify(jsonObject);
 					// this.ignoreWhenUpdated = true;
 					// this._doc.record(new ChangeSong(this._doc, jsonString), true, true);
-					this._doc.song.fromJsonObject(jsonObject);
+					this._doc.song.fromJsonObject(this.convertNotesToArray(jsonObject));
 					this._doc.notifier.changed();
-					this.currentSong = jsonObject;
+					// this.lastSentSong = jsonObject;
+					// console.log('last song:')
+					// w.printNotes(this.lastSentSong)
+					// w.detectHole()
+					// console.log('--- end recieve update.')
 				} else if (message.type == 'get-song') {
-					let jsonObject: Object = this._doc.song.toJsonObject(true, 1, true);
+					
+					let jsonObject: Object = this.getJsonSong()
 					let data = JSON.stringify({ user: this.username, type: 'set-song', song: jsonObject, date: Date.now() });
+					// console.log('--- send song:')
+					// w.printNotes(jsonObject)
 					this.socket.send(data);
+					// console.log('--- end send song.')
 				} else if (message.type == 'set-song' && !this.songIsSet) {
-					// let jsonString = JSON.stringify(message.song);
-					// this.ignoreWhenUpdated = true;
-					// this._doc.record(new ChangeSong(this._doc, jsonString), true, true);
+					// console.log('--- receive song:')
+					// w.printNotes(message.song)
 					this._doc.song.fromJsonObject(message.song);
 					this._doc.notifier.changed();
-					this.currentSong = message.song;
+					this.lastSentSong = this.convertNotesToObject(message.song);
 					this.songIsSet = true;
+					// console.log('--- end receive song.')
 				}
 			} catch (e) {
 
 			}
 		};
+	}
+
+	public getJsonSong(): Object {
+		return structuredClone(this._doc.song.toJsonObject(true, 1, true));
+	}
+
+	public convertNotesToObject(song: any): Object {
+		let jsonObject = structuredClone(song)
+		for(let channel of jsonObject.channels) {
+			for(let pattern of channel.patterns) {
+				let notesObject:any = {}
+				for(let i=0 ; i<pattern.notes.length ; i++) {
+					let note = pattern.notes[i]
+					notesObject[JSON.stringify(note)] = note;
+				}
+				pattern.notes = notesObject;
+			}
+		}
+		return jsonObject
+	}
+
+	public convertNotesToArray(song: any): Object {
+		let jsonObject = structuredClone(song)
+		for(let channel of jsonObject.channels) {
+			for(let pattern of channel.patterns) {
+				pattern.notes = Object.values(pattern.notes).sort((a:any, b:any)=> a.points[0].tick - b.points[0].tick);
+			}
+		}
+		return jsonObject
 	}
 
 	public whenUpdated = (): void => {
@@ -88,24 +135,39 @@ export class Communication {
 		// 	this.ignoreWhenUpdated = false;
 		// 	return;
 		// }
-
-		const jsonObject: Object = window.structuredClone(this._doc.song.toJsonObject(true, 1, true));
-
-		if (this.currentSong == null) {
-			this.currentSong = jsonObject;
+		// let w: any = window
+		// console.log('--- when update: ');
+		// w.detectHole()
+		let jsonObject: Object = this.getJsonSong();
+		// console.log('song:')
+		// w.printNotes(jsonObject)
+		// console.log('last song:')
+		// w.printNotes(this.lastSentSong)
+		jsonObject = this.convertNotesToObject(jsonObject);
+		if (this.lastSentSong == null) {
+			this.lastSentSong = jsonObject;
 			return;
 		}
 
-		let changes = DeepDiff.diff(this.currentSong, jsonObject);
-		if (changes == null) {
+		let changes = DeepDiff.diff(this.lastSentSong, jsonObject);
+		if(changes == null) {
+			return
+		}
+		let changesToKeep = [];
+		for(let change of changes) {
+			if(this.recievedChanges.findIndex((c)=>DeepDiff.diff(change, c) == null)<0) {
+				changesToKeep.push(change)
+			}
+		}
+		if (changesToKeep.length == 0) {
 			return;
 		}
 		if(this.updateTimeout != null) {
 			return
 		}
 
-		console.log('when updated: ', changes, jsonObject);
-		let data = JSON.stringify({ user: this.username, type: 'change', changes: changes, date: Date.now() });
+		// console.log('send update: ', changesToKeep, jsonObject);
+		let data = JSON.stringify({ user: this.username, type: 'change', changes: changesToKeep, date: Date.now() });
 		this.socket.send(data);
 
 		clearTimeout(this.updateTimeout as any)
@@ -113,8 +175,105 @@ export class Communication {
 			this.updateTimeout = null
 			this.whenUpdated()
 		}, 1000) as any
-
-		this.currentSong = jsonObject;
+		
+		this.lastSentSong = jsonObject;
+		// w.detectHole()
+		// console.log('--- end send update.');
 	}
 }
 //}
+
+let w: any = window
+let addNote = (notes: any, channel: number)=> {
+	let note = 40+Math.round(Math.random()*20)
+	let duration = 2; // Math.round(Math.random()*4)
+	let startTime = notes.length == 0 ? 0 : notes[notes.length-1].points[0].tick + duration; // Math.round(Math.random()*8)
+	console.log('add', channel, note, startTime, duration)
+	notes.push({pitches: [note], points: [{pitchBend: 0, tick: startTime, volume: 100}, {pitchBend: 0, tick: startTime+duration, volume:100}]})
+}
+
+w.detectHole = ()=> {
+	let jsonObject: any = structuredClone(w._doc.song.toJsonObject(true, 1, true));
+	let timebar = 0;
+	let duration = 2;
+	for(let channel of [0, 1]) {
+		let notes = jsonObject.channels[channel].patterns[0].notes
+		for(let note of notes) {
+			if(note.points[0].tick > timebar) {
+				w.stopCommunicationTest()
+				console.log('hole:')
+				w.printNotes(jsonObject)
+				console.log('hole!!!')
+			}
+			timebar += duration
+		}
+	}
+	
+}
+
+w.printNotes = (jsonObject: any)=> {
+	for(let channel of [0, 1]) {
+		let notes = jsonObject.channels[channel].patterns[0].notes;
+		let notesString = '';
+		if(notes instanceof Array) {
+			for(let i=0 ; i<notes.length ; i++) {
+				notesString += notes[i].pitches.join(',') + '|' + notes[i].points[0].tick + '-' + notes[i].points[1].tick + '  ';
+			}
+		} else {
+			for(let noteKey in notes) {
+				let note = notes[noteKey]
+				notesString += note.pitches.join(',') + '|' + note.points[0].tick + '-' + note.points[1].tick + '  ';
+			}
+		}
+		console.log('c' + channel + ': ' + notesString);
+	}
+}
+
+w.testCommunication = (channel: number)=> {
+	console.log('--- add note')
+	w.detectHole()
+	let jsonObject: any = structuredClone(w._doc.song.toJsonObject(true, 1, true));
+	w.printNotes(jsonObject)
+	let oldJsonObject: any = structuredClone(jsonObject);
+	let notes = jsonObject.channels[channel].patterns[0].notes
+	addNote(notes, channel)
+	if(notes[notes.length-1].points[0].tick>=30) {
+		console.log('filled.')
+		w.stopCommunicationTest()
+		return
+	}
+	// if(notes.length==0) {
+	// 	addNote(notes, channel)
+	// } else {
+	// 	if(Math.random()>0.25) {
+	// 		addNote(notes, channel)
+	// 	} else {
+	// 		let noteIndex = Math.floor(notes.length*Math.random())
+	// 		let note = notes[noteIndex]
+	// 		console.log('remove', channel, note.pitches[0], note.points[0].tick, note.points[1].tick)
+	// 		notes.splice(noteIndex, 1)
+	// 	}
+	// }
+
+	w._doc.song.fromJsonObject(jsonObject);
+	w._doc.notifier.changed();
+	w.communication.whenUpdated();
+
+	let newJsonObject: any = structuredClone(w._doc.song.toJsonObject(true, 1, true));
+	console.log('added notes, song is now:')
+	w.printNotes(newJsonObject)
+	let changes = DeepDiff.diff(oldJsonObject, newJsonObject);
+	if (changes == null) {
+		console.log('!!!no changes!!!')
+		w.stopCommunicationTest()
+		return
+	}
+
+	w.testTimeout = setTimeout(()=>w.testCommunication(channel), Math.random()*500)
+	w.detectHole()
+	console.log('--- end add note')
+}
+
+w.stopCommunicationTest = ()=> {
+	clearTimeout(w.testTimeout)
+}
